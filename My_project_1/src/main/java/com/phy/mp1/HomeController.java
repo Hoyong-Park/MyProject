a4c66000663f7aad90c62fc72aeafb76d158331f
PHY <PHY@PHY-laptop>
2024-05-13 16:19:24 +0900

    project start

---
+++ My_project_1/src/main/java/com/phy/mp1/HomeController.java
@@ -0,0 +1,18 @@
+package com.phy.mp1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String requestMethodName(HttpServletRequest request) {
+		request.setAttribute("cp", "home");
+		request.setAttribute("cpSub", "homeContent");
+		return "index";
+	}
+	
+}
